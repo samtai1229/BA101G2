06ddcda66bf626f1ec44ab7d223c26be7edbae11
voka1031 <voka1031@gmail.com>
2017-06-15 16:46:25 +0800

    偉濃 0615  - controller 2
    增加MotorModelServlet.java 
    從DB撈圖與把圖傳進DB還沒寫，所以新增/修改/查詢功能沒測。

--- src/com/motor_model/controller/MotorModelServlet.java
+++ src/com/motor_model/controller/MotorModelServlet.java
@@ -1,34 +1,39 @@
-package com.motor.controller;
+package com.motor_model.controller;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.motor.model.MotorService;
 import com.motor.model.MotorVO;
+import com.motor_model.model.MotorModelService;
+import com.motor_model.model.MotorModelVO;
 
-public class MotorServlet extends HttpServlet {
+public class MotorModelServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+ 
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doPost(req, res);
-		
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+ 
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
-		System.out.println("MotorServlet in");
+		System.out.println("MotorModelServlet in");
 	
         if ("insert".equals(action)) { // 來自motor backendIndex.jsp的請求  
-    		System.out.println("MotorServlet in insert-action");			
+    		System.out.println("MotorModelServlet in insert-action");			
 			List<String> errorMsgs = new LinkedList<String>();
 			// Store this set in the request scope, in case we need to
 			// send the ErrorPage view.
@@ -37,63 +42,60 @@ public class MotorServlet extends HttpServlet {
 			try {
 				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
 				String modtype = req.getParameter("modtype").trim();
-				String plateno = req.getParameter("plateno").trim();
-				String engno = req.getParameter("engno").trim();
-				String locno = req.getParameter("locno").trim();
-				String status = req.getParameter("status").trim();
-				String note = req.getParameter("note").trim();				
+				String brand = req.getParameter("brand").trim();
+				String displacementStr = req.getParameter("displacement");
+				String name = req.getParameter("name").trim();
+				String renpriceStr = req.getParameter("renprice");
+				String salepriceStr = req.getParameter("saleprice");	
+				
+				//上傳圖片??
 				
 				
-				//處理日期
-				String dateString = req.getParameter("manudate");
-				SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-				java.util.Date date;
-				java.sql.Timestamp manudate=null;
-				long longTime;
+				Integer displacement = null;
 				try {
-					date = (java.util.Date) sdf.parse(dateString);//String to Date
-					longTime = date.getTime(); //取long
-					manudate = new java.sql.Timestamp(longTime); //切為SQL專用格式	
-					System.out.println(longTime);
-				} catch (ParseException e) {
-					e.printStackTrace();
-					errorMsgs.add("請輸入日期!");
-				} //處理日期--end
+					displacement = new Integer(displacementStr);
+				} catch (Exception e) {
+					errorMsgs.add("排氣量格式不正確");
+				}
 				
-				
-				
-				Integer mile = null;
+				Integer renprice = null;
 				try {
-					mile = new Integer(req.getParameter("mile").trim());
-				} catch (NumberFormatException e) {
-					errorMsgs.add("里程數請寫數字");
+					renprice = new Integer(renpriceStr);
+				} catch (Exception e) {
+					errorMsgs.add("租賃價格格式不正確");
+				}
+				
+				Integer saleprice = null;
+				try {
+					saleprice = new Integer(salepriceStr);
+				} catch (Exception e) {
+					errorMsgs.add("出售價格格式不正確");
 				}
 
-				MotorVO motorVO = new MotorVO();
-				motorVO.setModtype(modtype);
-				motorVO.setPlateno(plateno);
-				motorVO.setEngno(engno);
-				motorVO.setLocno(locno);
-				motorVO.setManudate(manudate);
-				motorVO.setMile(mile);
-				motorVO.setNote(note);
-				motorVO.setStatus(status);
+				MotorModelVO mmVO = new MotorModelVO();
+				mmVO.setModtype(modtype);
+				mmVO.setBrand(brand);
+				mmVO.setDisplacement(displacement);
+				mmVO.setName(name);
+				mmVO.setRenprice(renprice);
+				mmVO.setSaleprice(saleprice);
 
+				
+				
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
-					req.setAttribute("motorVO", motorVO); // 含有輸入格式錯誤的VO物件,也存入req
+					req.setAttribute("mmVO", mmVO); // 含有輸入格式錯誤的VO物件,也存入req
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/backend/backendMotor.jsp");
+							.getRequestDispatcher("/backend/backendMotorModel.jsp");
 					failureView.forward(req, res);
 					return;
 				}
 				
 				/***************************2.開始新增資料***************************************/
-				MotorService motorSvc = new MotorService();
-				motorVO = motorSvc.addMotor(modtype, plateno, engno, manudate, mile, locno, status, note);
-				
+				MotorModelService mmSvc = new MotorModelService();
+				//mmVO = mmSvc.addMotorModel(brand, displacement, name, renprice, saleprice, motpic);
 				/***************************3.新增完成,準備轉交(Send the Success view)***********/
-				String url = "/backend/backendMotor.jsp";
+				String url = "/backend/backendMotorModel.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交?.jsp
 				successView.forward(req, res);				
 				
@@ -102,7 +104,7 @@ public class MotorServlet extends HttpServlet {
 				errorMsgs.add(e.getMessage());
 				System.out.println("err main insert catch in");
 				RequestDispatcher failureView = req
-						.getRequestDispatcher("/backend/backendMotor.jsp");
+						.getRequestDispatcher("/backend/backendMotorModel.jsp");
 				failureView.forward(req, res);
 			}
 		}//insert 'if' end
@@ -118,66 +120,61 @@ public class MotorServlet extends HttpServlet {
 			try {
 				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
 				String modtype = req.getParameter("modtype").trim();
-				String plateno = req.getParameter("plateno").trim();
-				String engno = req.getParameter("engno").trim();
-				String locno = req.getParameter("locno").trim();
-				String status = req.getParameter("status").trim();
-				String note = req.getParameter("note").trim();
-				String motno = req.getParameter("motno").trim();
+				String brand = req.getParameter("brand").trim();
+				String displacementStr = req.getParameter("displacement");
+				String name = req.getParameter("name").trim();
+				String renpriceStr = req.getParameter("renprice");
+				String salepriceStr = req.getParameter("saleprice");	
 				
-				//處理日期
-				String dateString = req.getParameter("manudate");
-				SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-				java.util.Date date;
-				java.sql.Timestamp manudate=null;
-				long longTime;
-				try {
-					date = (java.util.Date) sdf.parse(dateString);//String to Date
-					longTime = date.getTime(); //取long
-					manudate = new java.sql.Timestamp(longTime); //切為SQL專用格式	
-					System.out.println(longTime);
-				} catch (ParseException e) {
-					e.printStackTrace();
-					errorMsgs.add("請輸入日期!");
-					System.out.println("err date in");
-				} //處理日期--end
+				//上傳圖片??
 				
-			
-				Integer mile = null;
+				
+				Integer displacement = null;
 				try {
-					mile = new Integer(req.getParameter("mile").trim());
-				} catch (NumberFormatException e) {
-					System.out.println("err mile in");
-					errorMsgs.add("里程數請寫數字");
+					displacement = new Integer(displacementStr);
+				} catch (Exception e) {
+					errorMsgs.add("排氣量格式不正確");
+				}
+				
+				Integer renprice = null;
+				try {
+					renprice = new Integer(renpriceStr);
+				} catch (Exception e) {
+					errorMsgs.add("租賃價格格式不正確");
+				}
+				
+				Integer saleprice = null;
+				try {
+					saleprice = new Integer(salepriceStr);
+				} catch (Exception e) {
+					errorMsgs.add("出售價格格式不正確");
 				}
 
-				MotorVO motorVO = new MotorVO();
-				motorVO.setMotno(motno);
-				motorVO.setModtype(modtype);
-				motorVO.setPlateno(plateno);
-				motorVO.setEngno(engno);
-				motorVO.setLocno(locno);
-				motorVO.setManudate(manudate);
-				motorVO.setMile(mile);
-				motorVO.setNote(note);
-				motorVO.setStatus(status);
+				MotorModelVO mmVO = new MotorModelVO();
+				
+				mmVO.setModtype(modtype);
+				mmVO.setBrand(brand);
+				mmVO.setDisplacement(displacement);
+				mmVO.setName(name);
+				mmVO.setRenprice(renprice);
+				mmVO.setSaleprice(saleprice);
 				
 
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
-					req.setAttribute("motorVO", motorVO); // 含有輸入格式錯誤的VO物件,也存入req
+					req.setAttribute("mmVO", mmVO); // 含有輸入格式錯誤的VO物件,也存入req
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/backend/backendMotor.jsp");
+							.getRequestDispatcher("/backend/backendMotorModel.jsp");
 					failureView.forward(req, res);
 					return;
 				}
 				System.out.println("update start");
 				/***************************2.開始新增資料***************************************/
-				MotorService motorSvc = new MotorService();
-				motorVO = motorSvc.updateMotor(motno, modtype, plateno, engno, manudate, mile, locno, status, note);
+				MotorModelService mmSvc = new MotorModelService();
+				//mmVO = mmSvc.updateMotorModel(modtype, brand, displacement, name, renprice, saleprice, motpic);
 				
 				/***************************3.新增完成,準備轉交(Send the Success view)***********/
-				String url = "/backend/backendMotor.jsp";
+				String url = "/backend/backendMotorModel.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交?.jsp
 				successView.forward(req, res);				
 				
@@ -186,7 +183,7 @@ public class MotorServlet extends HttpServlet {
 				errorMsgs.add(e.getMessage());
 				System.out.println("err main update catch in");
 				RequestDispatcher failureView = req
-						.getRequestDispatcher("/backend/backendMotor.jsp");
+						.getRequestDispatcher("/backend/backendMotorModel.jsp");
 				failureView.forward(req, res);
 			}
 		}//update 'if' end
@@ -202,33 +199,33 @@ public class MotorServlet extends HttpServlet {
 
 			try {
 				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
-				String motno = req.getParameter("motno");
-				System.out.println(motno);
+				String modtype = req.getParameter("modtype");
+				System.out.println(modtype);
 			
-				if(motno==null){
+				if(modtype==null){
 					errorMsgs.add("請選擇待刪車輛資料");
 				}
 
-				MotorVO motorVO = new MotorVO();
-				motorVO.setMotno(motno);
+				MotorModelVO mmVO = new MotorModelVO();
+				mmVO.setModtype(modtype);
 			
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
 					System.out.println("delete_!errorMsgs.isEmpty_start");
-					req.setAttribute("motorVO", motorVO); // 含有輸入格式錯誤的empVO物件,也存入req
+					req.setAttribute("mmVO", mmVO); // 含有輸入格式錯誤的empVO物件,也存入req
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/backend/backendMotor.jsp");
+							.getRequestDispatcher("/backend/backendMotorModel.jsp");
 					failureView.forward(req, res);
 					return;
 				}
 				System.out.println("delete start");
 				
 				/***************************2.開始新增資料***************************************/
-				MotorService motorSvc = new MotorService();
-				motorSvc.deleteMotor(motno);
+				MotorModelService mmSvc = new MotorModelService();
+				mmSvc.deleteMotorModel(modtype);
 				
 				/***************************3.更改完成,準備轉交(Send the Success view)***********/
-				String url = "/backend/backendMotor.jsp";
+				String url = "/backend/backendMotorModel.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交?.jsp
 				successView.forward(req, res);				
 				
@@ -237,7 +234,7 @@ public class MotorServlet extends HttpServlet {
 				errorMsgs.add(e.getMessage());
 				System.out.println("err main delete catch in");
 				RequestDispatcher failureView = req
-						.getRequestDispatcher("/backend/backendMotor.jsp");
+						.getRequestDispatcher("/backend/backendMotorModel.jsp");
 				failureView.forward(req, res);
 			}
 		}//delete 'if' end
@@ -253,48 +250,47 @@ public class MotorServlet extends HttpServlet {
 
 			try {
 				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
-				String motno = req.getParameter("motno");
-				System.out.println(motno);
-				if (motno == null || (motno.trim()).length() == 0) {
-					errorMsgs.add("請輸入車輛編號");
+				String modtype = req.getParameter("modtype");
+				System.out.println(modtype);
+				if (modtype == null || (modtype.trim()).length() == 0) {
+					errorMsgs.add("請輸入車輛型號");
 				}
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/backend/backendMotor.jsp");
+							.getRequestDispatcher("/backend/backendMotorModel.jsp");
 					failureView.forward(req, res);
 					return;//程式中斷
 				}
 				
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/backend/backendMotor.jsp");
+							.getRequestDispatcher("/backend/backendMotorModel.jsp");
 					failureView.forward(req, res);
 					return;//程式中斷
 				}
 				
 				/***************************2.開始查詢資料*****************************************/
 				System.out.println("query-started");
-				MotorService motorSvc = new MotorService();
-				MotorVO motorQueryVO = motorSvc.findByPK(motno);
-				if (motorQueryVO == null) {
+				MotorModelService mmSvc = new MotorModelService();
+				MotorModelVO mmQueryVO = mmSvc.findByPK(modtype);
+				if (mmQueryVO == null) {
 					errorMsgs.add("查無資料");
 				}
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req
-							.getRequestDispatcher("/backend/backendMotor.jsp");
+							.getRequestDispatcher("/backend/backendMotorModel.jsp");
 					failureView.forward(req, res);
 					return;//程式中斷
 				}
 				
 				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
 				System.out.println("query-finished");
-				req.setAttribute("motorQueryVO", motorQueryVO); // 資料庫取出的VO物件,存入req
-				System.out.println("motnoVO.motorQueryVO:"+motorQueryVO.getMotno());
-				System.out.println("motnoVO.motorQueryVO:"+motorQueryVO.getStatus());
+				req.setAttribute("mmQueryVO", mmQueryVO); // 資料庫取出的VO物件,存入req
+				System.out.println("mmQueryVO.getModtype():"+mmQueryVO.getModtype());
 				
-				String url = "/backend/backendMotor.jsp";
+				String url = "/backend/backendMotorModel.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 Emp.jsp
 				successView.forward(req, res);
 
@@ -302,7 +298,7 @@ public class MotorServlet extends HttpServlet {
 			} catch (Exception e) {
 				errorMsgs.add("無法取得資料:" + e.getMessage());
 				RequestDispatcher failureView = req
-						.getRequestDispatcher("/backend/backendMotor.jsp");
+						.getRequestDispatcher("/backend/backendMotorModel.jsp");
 				failureView.forward(req, res);
 			}
 		}//delete 'if' end
