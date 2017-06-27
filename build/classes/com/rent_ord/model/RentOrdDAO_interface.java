708d8b45f7cdc73c59516e48d4c4fc4917c7a0b8
voka1031 <voka1031@gmail.com>
2017-06-27 10:50:48 +0800

    0627新版rent_ord model
    for後端還車系統功能
    目前交車/還車表單送出後:
    裝備/車輛/租單狀態切換OK、罰金計算OK，結束里程數、日期、備註OK
    都沒做錯誤驗證

---
+++ build/classes/com/rent_ord/model/RentOrdDAO_interface.java
@@ -0,0 +1,61 @@
+package com.rent_ord.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Set;
+
+import com.equipment.model.EquipmentVO;
+
+public interface RentOrdDAO_interface {
+	
+	public String differDateCalculator(String rentno);
+	
+	public void insert(RentOrdVO rental_orderVO);
+
+	public void update(RentOrdVO rental_orderVO);
+
+	public void delete(String rentno);
+
+	public RentOrdVO findByPrimaryKey(String rentno);
+
+	public List<RentOrdVO> getAll();
+
+	public Set<RentOrdVO> getRentalOrdersByStatus(String status);
+
+	public Set<RentOrdVO> getRentalOrdersByRentLoc(String slocno);
+
+	public Set<RentOrdVO> getRentalOrdersByReturnLoc(String rlocno);
+
+	public Set<RentOrdVO> getRentalOrdersByStartDatePrioid(Timestamp start_time, Timestamp end_time);
+
+	public Set<RentOrdVO> getRentalOrdersByEndDatePrioid(Timestamp start_time, Timestamp end_time);
+	
+	public Set<RentOrdVO> getRentalOrdersForLeaseView(String slocno);
+	
+	public Set<RentOrdVO> getRentalOrdersForReturnView(String rlocno);
+	
+	public Set<EquipmentVO> getEquipmentVOsByRentno(String rentno);
+	
+	public void updateEmtsStatusAfterAvailable(String emtno, String action);
+	
+	public void updateEmtsStatusAfterNoshow(String emtno, String action);
+	
+	public void updateRentOrdStatusAfterAvailable(String rentno, String note, String action);
+	
+	public void updateRentOrdStatusAfterNoshow(String rentno, String note, String action);
+	
+	public void updateMotorStatusAfterAvailable(String motno, String action);
+	
+	public void updateMotorStatusAfterNoshow(String motno, String action);
+	
+	public void updateRentOrdAfterNoreturn(String rentno, Integer milend, Timestamp returndate, 
+			Integer fine, String rank, String note, String action);
+	
+	public void updateRentOrdAfterOvertime(String rentno, Integer milend, Timestamp returndate, 
+			Integer fine, String rank, String note, String action);
+	
+	public void updateMotorAfterReturn(String motno, Integer mile, String action);
+	
+	public void updateEmtsAfterReturn(String emtno, String action);
+
+}
---
+++ build/classes/com/rent_ord/model/RentOrdVO.java
@@ -0,0 +1,157 @@
+package com.rent_ord.model;
+
+import java.sql.Timestamp;
+
+public class RentOrdVO implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String rentno;
+	private String memno;
+	private String motno;
+	private String slocno;
+	private String rlocno;
+	private Integer milstart;
+	private Integer milend;
+	private Timestamp filldate;
+	private Timestamp startdate;
+	private Timestamp enddate;
+	private Timestamp returndate;
+	private Integer fine;
+	private Integer total;
+	private String rank;
+	private String status;
+	private String note;
+
+	public RentOrdVO() {
+
+	}
+
+	public String getRentno() {
+		return rentno;
+	}
+
+	public void setRentno(String rentno) {
+		this.rentno = rentno;
+	}
+
+	public String getMemno() {
+		return memno;
+	}
+
+	public void setMemno(String memno) {
+		this.memno = memno;
+	}
+
+	public String getMotno() {
+		return motno;
+	}
+
+	public void setMotno(String motno) {
+		this.motno = motno;
+	}
+
+	public String getSlocno() {
+		return slocno;
+	}
+
+	public void setSlocno(String slocno) {
+		this.slocno = slocno;
+	}
+
+	public String getRlocno() {
+		return rlocno;
+	}
+
+	public void setRlocno(String rlocno) {
+		this.rlocno = rlocno;
+	}
+
+	public Integer getMilstart() {
+		return milstart;
+	}
+
+	public void setMilstart(Integer milstart) {
+		this.milstart = milstart;
+	}
+
+	public Integer getMilend() {
+		return milend;
+	}
+
+	public void setMilend(Integer milend) {
+		this.milend = milend;
+	}
+
+	public Timestamp getFilldate() {
+		return filldate;
+	}
+
+	public void setFilldate(Timestamp filldate) {
+		this.filldate = filldate;
+	}
+
+	public Timestamp getStartdate() {
+		return startdate;
+	}
+
+	public void setStartdate(Timestamp startdate) {
+		this.startdate = startdate;
+	}
+
+	public Timestamp getEnddate() {
+		return enddate;
+	}
+
+	public void setEnddate(Timestamp enddate) {
+		this.enddate = enddate;
+	}
+
+	public Timestamp getReturndate() {
+		return returndate;
+	}
+
+	public void setReturndate(Timestamp returndate) {
+		this.returndate = returndate;
+	}
+
+	public Integer getFine() {
+		return fine;
+	}
+
+	public void setFine(Integer fine) {
+		this.fine = fine;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+}
