7dee24f7fcba42ba9f897201c3f5b50dcef7261c
codeMuzukashii <blitz9211@msn.com>
2017-07-17 19:08:36 +0800

    0717

--- src/com/motor_disp_list/model/MotorDispListDAO_interface.java
+++ src/com/motor_disp_list/model/MotorDispListDAO_interface.java
@@ -1,9 +1,18 @@
 package com.motor_disp_list.model;
 
+import com.motor_model.model.MotorModelVO;
+
 public interface MotorDispListDAO_interface {
 
 	public MotorDispListVO findByPrimaryKeyDispatchNo(String mdno);
 
 	public MotorDispListVO findByPrimaryKeyMotorNo(String motno);
+
+	//以下為Hibernate用
+	void insertByHib(MotorDispListVO mdListVO);
+
+	void updateByHib(MotorDispListVO mdListVO);
+
+
 
 }
--- src/com/motor_disp_list/model/MotorDispListVO.java
+++ src/com/motor_disp_list/model/MotorDispListVO.java
@@ -1,14 +1,31 @@
 package com.motor_disp_list.model;
 
-public class MotorDispListVO implements java.io.Serializable {
+import java.io.Serializable;
 
-	private static final long serialVersionUID = 1L;
+import com.motor.model.MotorVO;
+import com.motor_dispatch.model.MotorDispatchVO;
+import com.motor_model.model.MotorModelVO;
 
+public class MotorDispListVO implements Serializable {
+	private static final long serialVersionUID = -5137444861039543022L;
+	
 	private String mdno;
 	private String motno;
-
+	private String modtype;
+	
+	private MotorDispatchVO motorDispatchVO;
+	private MotorVO motorVO;
+	
 	public MotorDispListVO() {
+		super();
+	}
 
+	public MotorDispatchVO getMotorDispatchVO() {
+		return motorDispatchVO;
+	}
+
+	public void setMotorDispatchVO(MotorDispatchVO motorDispatchVO) {
+		this.motorDispatchVO = motorDispatchVO;
 	}
 
 	public String getMdno() {
@@ -26,5 +43,23 @@ public class MotorDispListVO implements java.io.Serializable {
 	public void setMotno(String motno) {
 		this.motno = motno;
 	}
+
+	public String getModtype() {
+		return modtype;
+	}
+
+	public void setModtype(String modtype) {
+		this.modtype = modtype;
+	}
+
+	public MotorVO getMotorVO() {
+		return motorVO;
+	}
+
+	public void setMotorVO(MotorVO motorVO) {
+		this.motorVO = motorVO;
+	}
+	
+	
 
 }
