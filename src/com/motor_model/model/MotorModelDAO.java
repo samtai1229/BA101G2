a20cd0da645a3793c3735aa3b5320f9809c35855
codeMuzukashii <blitz9211@msn.com>
2017-07-17 19:10:12 +0800

    0717

--- src/com/motor_model/model/MotorModelDAO.java
+++ src/com/motor_model/model/MotorModelDAO.java
@@ -17,7 +17,12 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import com.motor.model.MotorVO;
+
+import hibernate.util.HibernateUtil;
 
 public class MotorModelDAO implements MotorModelDAO_interface {
 	// 一個應用程式中,針對一個資料庫 ,共用一個DataSource即可
@@ -48,7 +53,12 @@ public class MotorModelDAO implements MotorModelDAO_interface {
 	
 	private static final String FUZZY_SEARCH = 
 			"SELECT * FROM MOTOR_MODEL where MODTYPE LIKE ? or BRAND LIKE ? or DISPLACEMENT LIKE ? or NAME LIKE ? or RENPRICE LIKE ? or SALEPRICE LIKE ? ORDER BY MODTYPE";
-	
+
+	//以下為Hibernate用
+		private static final String GET_ALL_STMT = "from MotorModelVO order by MODTYPE";
+		private static final String FUZZY_SEARCH_BY_HIBERNATE ="FROM MotorModelVO where MOTNO LIKE ? or MODTYPE LIKE ? or PLATENO LIKE ? or ENGNO LIKE ? or MANUDATE LIKE ? or MILE LIKE ? or LOCNO LIKE ? or STATUS LIKE ? or NOTE LIKE ? or NAME LIKE ? or DISPLACEMENT LIKE ? or RENPRICE LIKE ? or SALEPRICE LIKE ? or BRAND LIKE ? ORDER BY MOTNO"; 
+//				"SELECT m.motno, m.modtype, d.name, d.displacement, d.renprice, d.brand m.plateno, m.engno, m.manudate, m.mile, m.locno, d.saleprice,m.status, m.note FROM MOTOR as m join motor_model as d ON m.modtype = d.modtype where m.MOTNO LIKE ? or m.MODTYPE LIKE ? or m.PLATENO LIKE ? or m.ENGNO LIKE ? or m.MANUDATE LIKE ? or m.MILE LIKE ? or m.LOCNO LIKE ? or m.STATUS LIKE ? or m.NOTE LIKE ? or d.NAME LIKE ? or d.DISPLACEMENT LIKE ? or d.RENPRICE LIKE ? or d.SALEPRICE LIKE ? or d.BRAND LIKE ? ORDER BY m.MOTNO";
+		
 
 	@Override
 	public void insert(MotorModelVO mmVO) {
@@ -401,6 +411,55 @@ System.out.println("mmDAOlist: " + list);
 		fis.close();
 
 		return baos.toByteArray();
+	}
+	
+	//以下為Hibernate用
+	@Override
+	public List<MotorModelVO> fuzzySearchByHib(String fuzzyValue) {
+		List<MotorModelVO> list = null;
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+
+			Query query = session.createQuery(FUZZY_SEARCH_BY_HIBERNATE);
+			
+			query.setParameter(0, "%"+fuzzyValue+"%");
+			query.setParameter(1, "%"+fuzzyValue+"%");
+			query.setParameter(2, "%"+fuzzyValue+"%");
+			query.setParameter(3, "%"+fuzzyValue+"%");
+			query.setParameter(4, "%"+fuzzyValue+"%");
+			query.setParameter(5, "%"+fuzzyValue+"%");
+			query.setParameter(6, "%"+fuzzyValue+"%");
+			query.setParameter(7, "%"+fuzzyValue+"%");
+			query.setParameter(8, "%"+fuzzyValue+"%");
+			query.setParameter(9, "%"+fuzzyValue+"%");
+			query.setParameter(10, "%"+fuzzyValue+"%");
+			query.setParameter(11, "%"+fuzzyValue+"%");
+			query.setParameter(12, "%"+fuzzyValue+"%");
+			query.setParameter(13, "%"+fuzzyValue+"%");
+			list = query.list();
+			session.getTransaction().commit();
+		} catch (RuntimeException ex) {
+			session.getTransaction().rollback();
+			throw ex;
+		}
+		return list;
+	}
+
+	@Override
+	public List<MotorModelVO> getAllByHib() {
+		List<MotorModelVO> list = null;
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery(GET_ALL_STMT);
+			list = query.list();
+			session.getTransaction().commit();
+		} catch (RuntimeException ex) {
+			session.getTransaction().rollback();
+			throw ex;
+		}
+		return list;
 	}
 
 }
